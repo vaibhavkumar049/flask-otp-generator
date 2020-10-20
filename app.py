33f05f3a5a7a683328270587a18003200455c0f3
28JayG <jaygandhi.32@outlook.com>
2020-10-21 03:39:37 +0530

    button click generates random number b/w 10^9 and 10^10

--- app.py
+++ app.py
@@ -1,17 +1,24 @@
 import random
+import flask
 from flask import Flask, render_template, url_for
 from form import SimpleForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
     form = SimpleForm()
-    return render_template('homepage.html', form=form)
+    generatedOTP= ''
+    if flask.request.method == 'GET':
+        generatedOTP = generate_random()
+    if flask.request.method == 'POST':
+        generatedOTP = ''
+
+    return render_template('homepage.html', form=form, generatedOTP=generatedOTP)
 
 def generate_random():
-    return random.randint(0,9)
+    return random.randint(10**9,10**10)
 
 if(__name__ == '__main__'):
     app.run(debug=True)
