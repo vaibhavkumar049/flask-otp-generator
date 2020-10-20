92189a1063df368244b703148faecd18b20f4409
28JayG <jaygandhi.32@outlook.com>
2020-10-21 03:14:03 +0530

    adds forms

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from flask import Flask
+import random
+from flask import Flask, render_template, url_for
+from form import SimpleForm
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 
 @app.route('/')
 def index():
-    return 'Click to generate OTP'
+    form = SimpleForm()
+    return render_template('homepage.html', form=form)
+
+def generate_random():
+    return random.randint(0,9)
 
 if(__name__ == '__main__'):
     app.run(debug=True)
